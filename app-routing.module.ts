05156e9844a9a5871b44bc4b64ea497b9ab537f1
Domenica Uyunkar <domeuyunkar@gmail.com>
2023-01-23 12:32:11 -0500

    ExamenQ1UD

---
+++ app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Componente1Component } from './componente1/componente1.component';
+import { Componente2Component } from './componente2/componente2.component';
+
+const routes: Routes = [
+  {path:'',component:Componente1Component},
+  {path:'2',component:Componente2Component},
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
